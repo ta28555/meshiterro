ef685e880c888a927ac3ca8a32f325df2b74c18e
ta28555 <no.yuuuuu@gmail.com>
2021-04-22 01:24:07 +0000

    [Finish]4-3

--- Gemfile
+++ Gemfile
@@ -63,3 +63,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # deviseをインストールする
 gem 'devise'
+
+# refileをインストール
+gem "refile", require: "refile/rails", github:'manfe/refile'
+gem "refile-mini_magick"
---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,34 @@
+class PostImagesController < ApplicationController
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params) # post_images#new
+    @post_image.user_id = current_user.id # deviseのヘルパーメソッド
+    @post_image.save
+    redirect_to post_images_path # post_images#index
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path # 一覧画面へ
+  end
+
+
+  private # createのストロングパラメータ
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+end
