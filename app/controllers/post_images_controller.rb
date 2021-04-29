c83f14d6c4336ec2482750418ba76b95831f83ac
ta28555 <no.yuuuuu@gmail.com>
2021-04-29 02:24:11 +0000

    [Finish]4-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,8 +7,11 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params) # post_images#new
     @post_image.user_id = current_user.id # deviseのヘルパーメソッド
-    @post_image.save
-    redirect_to post_images_path # post_images#index
+    if @post_image.save
+      redirect_to post_images_path # post_images#index
+    else
+      render :new
+    end
   end
 
   def index
